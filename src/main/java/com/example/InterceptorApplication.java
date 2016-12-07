3ff7fae9c35e2721c31b15ca2711ff7a5f9bf699
Dave Syer <dsyer@pivotal.io>
2016-12-07 09:44:24 +0000

    Remove annotation

--- src/main/java/com/example/InterceptorApplication.java
+++ src/main/java/com/example/InterceptorApplication.java
@@ -1,20 +1,20 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class InterceptorApplication {
-	
+
 	@RequestMapping("/")
 	public String home() {
 		return "Hello World";
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(InterceptorApplication.class, args);
+		new SpringApplicationBuilder(InterceptorApplication.class).run(args);
 	}
 }
