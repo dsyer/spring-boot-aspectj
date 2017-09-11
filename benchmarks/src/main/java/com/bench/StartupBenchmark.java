85054cc5729c2a69ec2ae9166dd281306ccce68b
Dave Syer <dsyer@pivotal.io>
2017-09-11 09:08:25 +0100

    Use @Param to reduce clutter

--- benchmarks/src/main/java/com/bench/StartupBenchmark.java
+++ benchmarks/src/main/java/com/bench/StartupBenchmark.java
@@ -21,6 +21,7 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
@@ -33,100 +34,70 @@ import org.openjdk.jmh.annotations.Warmup;
 public class StartupBenchmark {
 
 	@Benchmark
-	public void vanilla(ApplicationState state) throws Exception {
-		state.run();
-	}
-
-	@Benchmark
-	public void vanilla_1_50(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=50", "--bench.aspects=1");
-		state.run();
-	}
-
-	@Benchmark
-	public void vanilla_10_50(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=50", "--bench.aspects=10");
-		state.run();
-	}
-
-	@Benchmark
-	public void vanilla_20_50(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=50", "--bench.aspects=20");
-		state.run();
-	}
-
-	@Benchmark
-	public void vanilla_1_100(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=100", "--bench.aspects=1");
-		state.run();
-	}
-
-	@Benchmark
-	public void vanilla_10_100(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=100", "--bench.aspects=10");
-		state.run();
-	}
-
-	@Benchmark
-	public void vanilla_20_100(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=100", "--bench.aspects=20");
-		state.run();
-	}
-
-	@Benchmark
-	public void annotation_1_50(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=50", "--bench.aspects=1", "--bench.annotation=true");
-		state.run();
-	}
-
-	@Benchmark
-	public void annotation_10_50(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=50", "--bench.aspects=10", "--bench.annotation=true");
-		state.run();
-	}
-
-	@Benchmark
-	public void annotation_20_50(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=50", "--bench.aspects=20", "--bench.annotation=true");
-		state.run();
-	}
-
-	@Benchmark
-	public void annotation_1_100(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=100", "--bench.aspects=1", "--bench.annotation=true");
-		state.run();
-	}
-
-	@Benchmark
-	public void annotation_10_100(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=100", "--bench.aspects=10", "--bench.annotation=true");
-		state.run();
-	}
-
-	@Benchmark
-	public void annotation_20_100(ApplicationState state) throws Exception {
-		state.setProgArgs("--bench.beans=100", "--bench.aspects=20", "--bench.annotation=true");
+	public void spring(SpringState state) throws Exception {
 		state.run();
 	}
 
 	@Benchmark
 	public void ltw(ApplicationState state) throws Exception {
 		state.setProgArgs("--spring.aop.auto=false");
-		state.setJvmArgs(
-				"-javaagent:" + home() + "/.m2/repository/org/aspectj/aspectjweaver/1.8.10/aspectjweaver-1.8.10.jar");
+		state.setJvmArgs("-javaagent:" + home()
+				+ "/.m2/repository/org/aspectj/aspectjweaver/1.8.10/aspectjweaver-1.8.10.jar");
 		state.run();
 	}
 
 	@Benchmark
 	public void ltw_100(ApplicationState state) throws Exception {
 		state.setProgArgs("--bench.beans=100", "--spring.aop.auto=false");
-		state.setJvmArgs(
-				"-javaagent:" + home() + "/.m2/repository/org/aspectj/aspectjweaver/1.8.10/aspectjweaver-1.8.10.jar");
+		state.setJvmArgs("-javaagent:" + home()
+				+ "/.m2/repository/org/aspectj/aspectjweaver/1.8.10/aspectjweaver-1.8.10.jar");
 		state.run();
 	}
 
 	private String home() {
 		return System.getProperty("user.home");
+	}
+
+	@State(Scope.Benchmark)
+	public static class SpringState extends ProcessLauncherState {
+
+		public static enum Scale {
+			v0_10(0, 10), v1_10(1, 10), v1_100(1, 100), v10_50(10, 50), v20_50(20,
+					50), a0_10(0, 10, true), a1_10(1, 10, true), a1_100(1, 100,
+							true), a10_50(10, 50,
+									true), a10_100(10, 100, true), a20_50(20, 50, true);
+			private int beans;
+			private int aspects;
+			private boolean annotation;
+
+			private Scale(int aspects, int beans) {
+				this(beans, aspects, false);
+			}
+
+			private Scale(int aspects, int beans, boolean annotation) {
+				this.beans = beans;
+				this.aspects = aspects;
+				this.annotation = annotation;
+			}
+
+			public String[] getArgs() {
+				return new String[] { "--server.port=0", "--bench.aspects=" + aspects,
+						"--bench.beans=" + beans, "--bench.annotations=" + annotation };
+			}
+		}
+
+		@Param
+		private Scale scale = Scale.v1_10;
+
+		public SpringState() {
+			super("target");
+			setProgArgs(scale.getArgs());
+		}
+
+		@TearDown(Level.Iteration)
+		public void stop() throws Exception {
+			super.after();
+		}
 	}
 
 	@State(Scope.Benchmark)
